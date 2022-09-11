67aba439a11002a8a1b1c41e7fd2c5ec05e6617e
T-Hash06 <tomasfelipe.p@gmail.com>
2022-09-11 16:33:40 -0500

    [CREATE] Button Component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import "../styles/Button.css";
+
+interface props {
+	onClick: () => void;
+	className: string;
+	children: string;
+	main?: boolean;
+}
+
+export default function Button({ onClick, children, className, main }: props) {
+	return (
+		<>
+			<button
+				className={`button-container ${className} ${
+					main ? "main" : ""
+				}`}
+				onClick={onClick}
+			>
+				{children}
+			</button>
+		</>
+	);
+}
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,13 +1,16 @@
 import "../styles/LoginView.css";
 
-import Title from "../components/Title";
 import TextInput from "../components/TextInput";
+import Button from "../components/Button";
+import Title from "../components/Title";
 
 import { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginView() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
 
 	const handleUsername = (event: ChangeEvent<HTMLInputElement>) => {
 		setUsername(event.target.value);
@@ -16,21 +19,36 @@ export default function LoginView() {
 		setPassword(event.target.value);
 	};
 
+	const handleLogin = () => {
+		alert("Unimplemented");
+	};
+	const handleRegister = () => {
+		navigate("/register");
+	};
+
 	return (
 		<>
 			<div id="login-container">
 				<Title>Login</Title>
 				<section className="inputs-section">
 					<TextInput
-						className={`text-input`}
+						className="text-input"
 						label="username"
 						onChange={handleUsername}
 					/>
 					<TextInput
-						className={`text-input`}
+						className="text-input"
 						label="password"
 						onChange={handlePassword}
 					/>
+				</section>
+				<section className="buttons-section">
+					<Button className="rect-button" onClick={handleLogin} main>
+						Login
+					</Button>
+					<Button className="rect-button" onClick={handleRegister}>
+						Register
+					</Button>
 				</section>
 			</div>
 		</>
