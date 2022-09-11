c61826fe0d299d92abfd9d4ceac6714fa7b56756
T-Hash06 <tomasfelipe.p@gmail.com>
2022-09-11 16:04:09 -0500

    [CREATE] Text Input Component

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,32 @@
+import "../styles/TextInput.css";
+
+import { ChangeEvent, useState } from "react";
+
+interface props {
+	onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+	className?: string;
+	label: string;
+}
+
+export default function TextInput({ onChange, className, label }: props) {
+	const [isFilled, setFilled] = useState(false);
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const value = event.target.value;
+		setFilled(!!value);
+		onChange(event);
+	};
+
+	return (
+		<>
+			<div
+				className={`input-container ${className} ${
+					isFilled ? `filled` : ""
+				}`}
+			>
+				<input className="input" type="text" onChange={handleChange} />
+				<label className="label">{label}</label>
+			</div>
+		</>
+	);
+}
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,12 +1,37 @@
 import "../styles/LoginView.css";
 
 import Title from "../components/Title";
+import TextInput from "../components/TextInput";
+
+import { ChangeEvent, useState } from "react";
 
 export default function LoginView() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleUsername = (event: ChangeEvent<HTMLInputElement>) => {
+		setUsername(event.target.value);
+	};
+	const handlePassword = (event: ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.target.value);
+	};
+
 	return (
 		<>
 			<div id="login-container">
 				<Title>Login</Title>
+				<section className="inputs-section">
+					<TextInput
+						className={`text-input`}
+						label="username"
+						onChange={handleUsername}
+					/>
+					<TextInput
+						className={`text-input`}
+						label="password"
+						onChange={handlePassword}
+					/>
+				</section>
 			</div>
 		</>
 	);
